c2d46157a980f792e2937df7355a548aa5aa7fd1
Zach Bochanski <ztbochanski@gmail.com>
2023-03-15 16:18:11 -0700

    refactor list to only return streambuilder widget to update post list state

--- lib/widgets/post_list.dart
+++ lib/widgets/post_list.dart
@@ -13,46 +13,25 @@ class PostList extends StatefulWidget {
 class _PostListState extends State<PostList> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Posts')),
-      body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
-              DateFormat dateFormat = DateFormat('EEEE, MMMM d, yyyy');
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+            DateFormat dateFormat = DateFormat('EEEE, MMMM d, yyyy');
 
-              return ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    var post = snapshot.data!.docs[index];
-                    return ListTile(
-                        title: Text(dateFormat.format(post['date'].toDate())),
-                        subtitle: Text(post['quantity'].toString()));
-                  });
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error $snapshot.error'));
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          }),
-      floatingActionButton: const NewEntryButton(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
-  }
-}
-
-class NewEntryButton extends StatelessWidget {
-  const NewEntryButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('posts')
-              .add({'date': 'Fake Date', 'quantity': 3});
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  var post = snapshot.data!.docs[index];
+                  return ListTile(
+                      title: Text(dateFormat.format(post['date'].toDate())),
+                      subtitle: Text(post['quantity'].toString()));
+                });
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error $snapshot.error'));
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
         });
   }
 }
