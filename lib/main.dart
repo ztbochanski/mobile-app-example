1c904e5853bb92a56d7120806fc0e187f1dff1d9
Zach Bochanski <ztbochanski@gmail.com>
2023-03-15 16:50:07 -0700

    change firebase options import to full path

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:wasteagram/firebase_options.dart';
 
 import 'package:wasteagram/app.dart';
 
