103a4c4122aa47d36ae8fc1ed79ca274724bd7a1
Zach Bochanski <ztbochanski@gmail.com>
2023-03-15 16:19:57 -0700

    refactor to use scaffold logic only

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:wasteagram/widgets/app_scaffold.dart';
+import 'package:wasteagram/widgets/post_list.dart';
+
 class ListScreen extends StatelessWidget {
+  const ListScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('List Screen'),
-      ),
-      body: ListView.builder(
-        itemCount: 100,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Item $index'),
-          );
-        },
-      ),
+    return const AppScaffold(
+      title: 'Posts',
+      floatingActionButton: NewEntryButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      child: PostList(),
     );
+  }
+}
+
+class NewEntryButton extends StatelessWidget {
+  const NewEntryButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('posts')
+              .add({'date': 'Fake Date', 'quantity': 3});
+        });
   }
 }
