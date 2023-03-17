cfe492583b7ef545e7681d418cd54a7eb4ab3391
Zach Bochanski <ztbochanski@gmail.com>
2023-03-16 20:45:16 -0700

    refactor post to non final memeber vars
    add to map method
    remove string formatting
    add date foremeat method

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,4 +1,3 @@
-import 'dart:ffi';
 import 'package:intl/intl.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,12 +5,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 /// Acts a data access object (DAO) for the managing posts in the database or as
 /// data transfer object (DTO) for transferring data between screens (UI).
 class Post {
-  final String id;
-  final String date;
-  final String imageURL;
-  final String latitude;
-  final String longitude;
-  final String quantity;
+  String id;
+  DateTime date;
+  String imageURL;
+  double latitude;
+  double longitude;
+  int quantity;
 
   Post({
     required this.id,
@@ -22,12 +21,26 @@ class Post {
     required this.quantity,
   });
 
+  /// Returns the date in a formatted string (e.g. "January 1, 2021")
+  String get formattedDate => DateFormat.yMMMMEEEEd().format(date);
+
+  /// Converts the post to a map for saving to the database.
+  Map<String, dynamic> toMap() {
+    return {
+      'date': date,
+      'imageURL': imageURL,
+      'latitude': latitude,
+      'longitude': longitude,
+      'quantity': quantity,
+    };
+  }
+
+  /// Converts the snapshot to a post object for accessing the data.
   Post.fromDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc)
       : id = doc.id,
-        date = DateFormat('EEEE, MMMM d, yyyy')
-            .format(doc.data()!['date'].toDate()),
+        date = (doc.data()!['date'] as Timestamp).toDate(),
         imageURL = doc.data()!['imageURL'],
-        latitude = doc.data()!['latitude'].toString(),
-        longitude = doc.data()!['longitude'].toString(),
-        quantity = doc.data()!['quantity'].toString();
+        latitude = doc.data()!['latitude'],
+        longitude = doc.data()!['longitude'],
+        quantity = doc.data()!['quantity'];
 }
