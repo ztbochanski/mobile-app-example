a0894cc2c8255a0b35d3cf5eba67a43dfca4443c
Zach Bochanski <ztbochanski@gmail.com>
2023-03-17 16:10:09 -0700

    refactor stream builder logic into methods

---
+++ lib/widgets/post_list_view.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:wasteagram/services/firestore_service.dart';
+import 'package:wasteagram/models/post.dart';
+import 'package:wasteagram/models/posts_list.dart';
+
+class PostListView extends StatefulWidget {
+  const PostListView({Key? key}) : super(key: key);
+
+  @override
+  State<PostListView> createState() => _PostListViewState();
+}
+
+class _PostListViewState extends State<PostListView> {
+  final FirestoreService _firestoreService = FirestoreService.getInstance();
+
+  Stream<QuerySnapshot> _getPostsStream() {
+    return _firestoreService.postsStream();
+  }
+
+  PostsList _getPostsList(QuerySnapshot snapshot) {
+    return PostsList.fromFirestore(snapshot);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _getPostsStream(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasData) {
+          PostsList postsList = _getPostsList(snapshot.data!);
+          return ListView.builder(
+            itemCount: postsList.length(),
+            itemBuilder: (context, index) {
+              var post = postsList.posts[index];
+              return ListTile(
+                  title: Text(post.formattedDate.toString()),
+                  subtitle: Text(post.quantity.toString()));
+            },
+          );
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('An error occurred'));
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
