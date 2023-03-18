263d92cd750b289a9ffc4fc3578a199e84a592cb
Zach Bochanski <ztbochanski@gmail.com>
2023-03-17 17:20:31 -0700

    refactor to accept async type

--- lib/models/posts_list.dart
+++ lib/models/posts_list.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:wasteagram/models/post.dart';
 
 /// A model for a list of posts. This class builds a list of posts using the post model.
@@ -8,9 +9,10 @@ class PostsList {
   PostsList(this.posts);
 
   /// A factory method for creating a `PostList` of `Posts` from a Firestore query snapshot.
-  factory PostsList.fromFirestore(QuerySnapshot snapshot) {
+  factory PostsList.fromFirestore(
+      AsyncSnapshot<QuerySnapshot<Object?>> snapshot) {
     List<Post> posts =
-        snapshot.docs.map((doc) => Post.fromFirestore(doc)).toList();
+        snapshot.data!.docs.map((doc) => Post.fromFirestore(doc)).toList();
     return PostsList(posts);
   }
 
