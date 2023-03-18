c6c8ed65c7260731534ed75718c1e0f481353bf4
Zach Bochanski <ztbochanski@gmail.com>
2023-03-17 17:22:39 -0700

    organize imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:wasteagram/firebase_options.dart';
-
 import 'package:wasteagram/app.dart';
 import 'package:wasteagram/services/firestore_service.dart';
 import 'package:wasteagram/services/location_service.dart';
