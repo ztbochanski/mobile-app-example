acc441fa39906b43a57ca2a76cc35ba6dea9204a
Zach Bochanski <ztbochanski@gmail.com>
2023-03-18 22:48:49 -0700

    add storage service init

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:flutter/services.dart';
 import 'package:wasteagram/firebase_options.dart';
 import 'package:wasteagram/app.dart';
 import 'package:wasteagram/services/firestore_service.dart';
+import 'package:wasteagram/services/image_picker_service.dart';
 import 'package:wasteagram/services/location_service.dart';
+import 'package:wasteagram/services/storage_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +22,9 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await FirestoreService.initialize();
+  await ImagePickerService.initialize();
   await LocationService.initialize();
+  await StorageService.initialize();
 
   runApp(const App());
 }
