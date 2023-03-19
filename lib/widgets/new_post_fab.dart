59d30cb89bcf867e22f02341f27575a23c6d9af9
Zach Bochanski <ztbochanski@gmail.com>
2023-03-18 21:53:55 -0700

    add semantics and onpressed logic

--- lib/widgets/new_post_fab.dart
+++ lib/widgets/new_post_fab.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:wasteagram/screens/new_post_screen.dart';
+import 'package:wasteagram/services/image_picker_service.dart';
 
-class NewPostFAB extends StatelessWidget {
+class NewPostFAB extends StatefulWidget {
   const NewPostFAB({super.key});
 
+  @override
+  State<NewPostFAB> createState() => _NewPostFABState();
+}
+
+class _NewPostFABState extends State<NewPostFAB> {
+  final ImagePickerService _pickerService = ImagePickerService.getInstance();
   @override
   Widget build(BuildContext context) {
     return Semantics(
@@ -12,12 +21,11 @@ class NewPostFAB extends StatelessWidget {
       onTapHint: 'Add a new post photo',
       child: FloatingActionButton(
         onPressed: () async {
-          final image = await ImagePicker.pickImage(source: ImageSource.camera);
-          // if (image != null) {
-          //   Navigator.of(context).push(MaterialPageRoute(
-          //       builder: (context) => NewPostScreen(image: image)));
-          // }
-          print(image);
+          final File file = await _pickerService.pickImageFromGallery();
+          if (!mounted) return;
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return NewPostScreen(file: file);
+          }));
         },
         tooltip: 'Add a new post photo',
         child: const Icon(Icons.camera_alt),
