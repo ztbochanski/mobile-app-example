63629507c428a1dbdea141f723ec1f866421d285
Zach Bochanski <ztbochanski@gmail.com>
2023-03-18 22:51:49 -0700

    add from to populate new post screen

---
+++ lib/widgets/new_post_form.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class NewPostForm extends StatefulWidget {
+  final String url;
+
+  const NewPostForm({required this.url, Key? key}) : super(key: key);
+
+  @override
+  State<NewPostForm> createState() => _NewPostFormState();
+}
+
+class _NewPostFormState extends State<NewPostForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _quantityController = TextEditingController();
+
+  @override
+  void dispose() {
+    _quantityController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Post'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 350,
+              width: 600,
+              child: Image.network(
+                widget.url,
+                fit: BoxFit.cover,
+                loadingBuilder: (BuildContext context, Widget child,
+                    ImageChunkEvent? loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+            TextFormField(
+              controller: _quantityController,
+              decoration: const InputDecoration(
+                hintText: 'Number of items',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a number';
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  Navigator.pop(context, _quantityController.text);
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
