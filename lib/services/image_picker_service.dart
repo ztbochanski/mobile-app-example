ed7ab2ac79f1a95acc42efebaf32ee39c4d98f9c
Zach Bochanski <ztbochanski@gmail.com>
2023-03-18 21:54:57 -0700

    add pickImageFromGallery method

---
+++ lib/services/image_picker_service.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+/// Singleton class for picking images from the gallery
+class ImagePickerService {
+  final ImagePicker _picker = ImagePicker();
+
+  static late ImagePickerService _instance;
+
+  /// Singleton constructor for [ImagePickerService]
+  ImagePickerService._() {
+    _instance = this;
+  }
+
+  factory ImagePickerService.getInstance() {
+    return _instance;
+  }
+
+  static Future<void> initialize() async {
+    _instance = ImagePickerService._();
+  }
+
+  /// returns a [File] object from the image picked from the gallery
+  Future<File> pickImageFromGallery() async {
+    final XFile? pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+    return File(pickedFile!.path);
+  }
+}
